86208238c3348df30fb9f389fe09f2c8c05885fd
Leandro Vaca <leandronvc123@gmail.com>
2026-08-21 12:29:51 -0300

    Attempt to fix the slider
    fuck me

--- src/subcomponents/heroSlider/heroSlider.jsx
+++ src/subcomponents/heroSlider/heroSlider.jsx
@@ -1,11 +1,16 @@
 import './heroSlider.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 const HeroSlider = ({ imgs, interval = 5000 }) => {
-  
+
   const [ currentIndex, setCurrentIndex ] = useState(0);
   const [ nextIndex, setNextIndex ] = useState(1);
   const [ fade, setFade ] = useState(false)
+
+  const imgsRef = useRef(imgs);
+  useEffect(() => {
+    imgsRef.current = imgs
+  }, [imgs]);
 
   useEffect(() => {
     const preloadImages = () => {
@@ -16,24 +21,30 @@ const HeroSlider = ({ imgs, interval = 5000 }) => {
     };
     preloadImages()
   },[imgs])
-  
+
   useEffect(() => {
+    let fadeTimeout;
+    
     const timer = setInterval(() => {
       setFade(true);
-      setTimeout(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % imgs.length);
-        setNextIndex((prevIndex) => (prevIndex + 1) % imgs.length);
-        setFade(false);
+      fadeTimeout = setTimeout(() => {
+        const len = imgsRef.current.length
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % len);
+        setNextIndex((prevIndex) => (prevIndex + 1) % len);
+        setFade(false);      
       }, interval / 2)
     }, interval)
 
-    return () => clearInterval(timer)
-  }, [imgs, interval])
+    return () => {
+      clearInterval(timer)
+      clearTimeout(fadeTimeout)
+    }
+  }, [interval])
 
   return (
     <div className='hero__slider'>
       <span className='slider__overlay'/>
-      <div 
+      <div
         className={`slide ${fade ? 'fade__out' : 'fade__in'}`}
         style={{ backgroundImage: `url(${imgs[currentIndex]})` }}
       />
